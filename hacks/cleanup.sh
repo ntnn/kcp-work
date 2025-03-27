c2808add0ef7a039331796b3f642ef2451f9f75c
Nelo-T. Wallus <n.wallus@sap.com>
2025-03-27 12:18:47 +0100

    temp

--- hacks/cleanup.sh
+++ hacks/cleanup.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 
-kind delete clusters "$(kind get clusters)"
+kind delete clusters $(kind get clusters)
 
 (
     cd "$(dirname "$0")/.."
-    git clean -dX
+    git clean -fdX
 )
