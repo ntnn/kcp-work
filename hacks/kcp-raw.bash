77e8c83528c1e2e965a247bc51606da9e88cd140
Nelo-T. Wallus <n.wallus@sap.com>
2025-07-17 08:39:13 +0200

    Update kcp script
    Signed-off-by: Nelo-T. Wallus <n.wallus@sap.com>

--- hacks/kcp-raw.bash
+++ hacks/kcp-raw.bash
@@ -7,33 +7,40 @@ basedir="$(pwd)"
 echo refreshing etcd
 ./hacks/etcd.bash clean-data
 
+rm -rf "$basedir/.kcp"
+mkdir -p "$basedir/.kcp"
+
+extra_args=()
+
+log_level=2
+dlv=false
+for arg in "$@"; do
+    case "$arg" in
+        (log_level=*) log_level=${arg##log_level=};;
+        (dlv) dlv=true;;
+        (*) extra_args+=( "$arg" )
+    esac
+done
 
 kcp_args=(
     "start"
     --root-directory="$basedir/.kcp"
-    --etcd-servers='http://localhost:2380'
-    -v=2
+    --etcd-servers='http://localhost:2379'
+    -v=${log_level}
 
     --bind-address=127.0.0.1
-    --tls-cert-file=/opt/homebrew/etc/pki/issued/kcp.crt
-    --tls-private-key-file=/opt/homebrew/etc/pki/private/kcp.key
+    # --tls-cert-file=/opt/homebrew/etc/pki/issued/kcp.crt
+    # --tls-private-key-file=/opt/homebrew/etc/pki/private/kcp.key
+
+    --feature-gates=CacheAPIs=true,WorkspaceMounts=true
 )
 
-dlv=false
-for arg in "$@"; do
-    case "$arg" in
-        (dex)
-            kcp_args+=(
-            )
-            ;;
-        (dlv) dlv=true;;
-    esac
-done
-
+echo "starting kcp with '${kcp_args[@]}'"
+rm -f kcp.log
 {
     if [[ "$dlv" == true ]]; then
-        dlv debug --listen 127.0.0.1:9999 --headless ./kcp/cmd/kcp -- ${kcp_args[@]}
+        dlv debug --listen 127.0.0.1:9999 --headless ./kcp/cmd/kcp -- ${kcp_args[@]} ${extra_args[@]}
     else
-        go run ./kcp/cmd/kcp ${kcp_args[@]}
+        go run ./kcp/cmd/kcp ${kcp_args[@]} ${extra_args[@]}
     fi
 } 2>&1 | tee "$basedir/kcp.log"
