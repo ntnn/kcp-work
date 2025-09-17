bd8c7db3b26a6ca73d344f00f8a8aef002234286
Nelo-T. Wallus <n.wallus@sap.com>
2025-09-17 08:54:09 +0200

    cleanup
    Signed-off-by: Nelo-T. Wallus <red.brush9525@fastmail.com>
    Signed-off-by: Nelo-T. Wallus <n.wallus@sap.com>

--- hacks/cleanup.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-
-kind get clusters | xargs -rt kind delete clusters
-find "$(dirname "$0")/.." -name "*.kubeconfig" -o -name 'kubeconfig.yaml' -o -name 'kcp.admin-token' | xargs -rt rm
