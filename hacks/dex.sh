c98bc7cf7bf37067becce7a3c3dffaeb7939792d
Nelo-T. Wallus <n.wallus@sap.com>
2025-09-17 08:51:07 +0200

    Ephemeral dex
    Signed-off-by: Nelo-T. Wallus <red.brush9525@fastmail.com>
    Signed-off-by: Nelo-T. Wallus <n.wallus@sap.com>

--- hacks/dex.sh
+++ hacks/dex.sh
@@ -5,12 +5,9 @@ cd "$(dirname $0)/.."
 docker kill dex
 docker pull ghcr.io/dexidp/dex
 
-mkdir -p .dex
-
 docker run --name dex -it --rm --detach \
     -p 5557:5557 \
     -v ./hacks/dex.yaml:/dex.yaml:ro \
-    -v ./.dex:/var/lib/dex:rw \
     -v /opt/homebrew/etc/pki:/pki:ro \
     ghcr.io/dexidp/dex \
     dex serve /dex.yaml
